0703fb3d49fbc835b2262cb92afb4984f6de3936
miradonis <miradonis@web.de>
2022-10-05 17:06:51 +0200

    added task

--- JS-variables/assets/js/main.js
+++ JS-variables/assets/js/main.js
@@ -1,18 +1,27 @@
 "use strict";
 
-let carName = "BMW";
-console.log(carName);
+// let carName = "BMW";
+// console.log(carName);
 
-let x = 150;
-console.log(x);
+// let x = 150;
+// console.log(x);
 
-let y = 50;
-console.log(y);
+// let y = 50;
+// console.log(y);
 
-let z = x + y;
-console.log(z);
+// let z = x + y;
+// console.log(z);
 
-let firstName = "John",
-  lastName = "Doe",
-  age = 35;
-console.log(firstName, lastName, age);
+// let firstName = "John",
+//   lastName = "Doe",
+//   age = 35;
+// console.log(firstName, lastName, age);
+
+console.log("-------------------------------");
+let x = 34;
+x = 67;
+//let kann überschrieben werden
+
+const y = 34;
+y = 67;
+// const kann nicht überschrieben werden
